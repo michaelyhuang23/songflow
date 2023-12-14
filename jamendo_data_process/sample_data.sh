1ad9779bcd3f1b4eb48c8b56b0d8cc19668ab0b8
Michael Huang <michaelyhuang23@gmail.com>
2023-12-14 01:23:02 -0500

    fix copier

--- jamendo_data_process/sample_data.sh
+++ jamendo_data_process/sample_data.sh
@@ -4,20 +4,29 @@
 SOURCE_DIR="../full_processed_jamendo_data"
 DEST_DIR="../processed_jamendo_data"
 
-# List of directories to move
-DIRS=("01" "53" "86" "38" "79")
+# List of directories to copy
+DIRS=("08" "58" "90" "50" "82")
 
-# Loop through each directory and move its contents
-for dir in "${DIRS[@]}"; do
+# Ensure the destination directory exists
+mkdir -p "$DEST_DIR"
+
+# Loop through each directory and copy its contents
+for i in "${!DIRS[@]}"; do
+    dir=${DIRS[$i]}
     # Check if the directory exists
     if [ -d "$SOURCE_DIR/$dir" ]; then
-        echo "Moving files from $SOURCE_DIR/$dir to $DEST_DIR"
-        # Move the files
-        mv "$SOURCE_DIR/$dir"/* "$DEST_DIR"/
+        echo "Copying files from $SOURCE_DIR/$dir to $DEST_DIR"
+
+        # Copy each file and rename it
+        for file in "$SOURCE_DIR/$dir"/*; do
+            filename=$(basename "$file")
+            extension="${filename##*.}"
+            new_filename="${i}.${extension}"
+            cp "$file" "$DEST_DIR/$new_filename"
+        done
     else
         echo "Directory $SOURCE_DIR/$dir does not exist"
     fi
 done
 
-echo "File move complete."
-
+echo "File copying and renaming complete."
