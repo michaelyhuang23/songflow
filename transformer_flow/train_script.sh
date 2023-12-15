89268e41557c72dd4d14e54169492b956e0014d6
Michael Huang <michaelyhuang23@gmail.com>
2023-12-15 06:25:18 -0500

    2 epoch

--- transformer_flow/train_script.sh
+++ transformer_flow/train_script.sh
@@ -7,4 +7,4 @@
 
 module load anaconda/2023a
 
-python train.py --dataset square_mel_jamendo_data --epochs 1 --batch_size 5 --lr 3e-3 --num_layers 10 --dim_feedforward 128
+python train.py --dataset square_mel_jamendo_data --epochs 2 --batch_size 5 --lr 3e-3 --num_layers 10 --dim_feedforward 128
